1e45f97b1b36f0b28d49f523750864efedb1b53c
SourabhR23 <rasal.sourabh23@gmail.com>
2021-01-29 00:42:02 +0530

    update

--- 06_Message_Flashing/tut_6.py
+++ 06_Message_Flashing/tut_6.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, session, redirect, url_for, render_template
+from flask import Flask, request, session, redirect, url_for, render_template, flash
 from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
@@ -20,10 +21,12 @@ def login():
         user = request.form['nm']
         # store the session data
         session['user'] = user
+        flash("Login Successful!")
         return redirect(url_for('user'))
     else:
         # if you are already logged in
         if 'user' in session:
+            flash("Already Logged In!")
             return redirect(url_for('user'))
 
         return render_template('login.html')
@@ -34,13 +37,18 @@ def user():
     if 'user' in session:
         # retrieve the session data
         user = session['user']
-        return f"<h1> Hello! {user}</h1>"
+        return render_template('user.html', user=user)
     else:
+        flash("You are not Logged In!")
         return redirect(url_for('login'))
 
 
 @app.route("/logout")
 def logout():
+    if 'user' in session:
+        user = session['user']
+        # this will flash the 'message' on 'login' page
+        flash(f"You have been logged out, {user}", "info")
     session.pop('user', None)
     return redirect(url_for('login'))
 
